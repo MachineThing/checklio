23d1a72ec1dd87e03a99f4cb4b09b8cbbeffbfc7
Mason Fisher <sackzombie10@gmail.com>
2023-01-04 08:36:15 -0500

    Checkbox is now animated and a lot nicer!

--- frontend/src/list.jsx
+++ frontend/src/list.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-export const Item = () => {
+export const Item = (props) => {
     return(
-        <li className="list-group-item"><label className="listItem"><input type="checkbox"></input><span></span> Cool item A</label></li>
+        <li className="list-group-item">
+            <label className="listItem">
+                <input type="checkbox"></input>
+                <span className="check-border">
+                    <span className="check-back"></span>
+                    <svg viewBox="0 0 24 24">
+                        <path d="M4.1 12.7L9 17.6 20.3 6.3" fill="none" />
+                    </svg>
+                </span>
+                <p>{props.children}</p>
+            </label>
+        </li>
     )
 }
 
@@ -11,11 +22,11 @@ export const List = () => {
         <div className="col-md-8 offset-md-2">
             <ul className="list-group">
                 <li className="list-group-item">My list</li>
-                <Item />
-                <Item />
-                <Item />
-                <Item />
-                <Item />
+                <Item>Create this app</Item>
+                <Item>Make an example list</Item>
+                <Item>Make checkbox look better</Item>
+                <Item>Remove bugs</Item>
+                <Item>Add less filter</Item>
             </ul>
         </div>
     )
