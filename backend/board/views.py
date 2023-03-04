a3bdc29172c131e61c28d7c6961ae1eeae9552ed
Mason Fisher <sackzombie10@gmail.com>
2023-03-03 21:31:12 -0500

    Adding model!
    Note: This is the point where django_migration is complete

--- backend/board/views.py
+++ backend/board/views.py
@@ -1,11 +1,25 @@
 import json
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from .models import Item
 
+# Note: this view shouldn't be csrf_exempt in the final product
+@csrf_exempt
 def core(request):
-    data_query = Item.objects.all().values()
-    data = json.dumps(list(data_query))
+    if request.method == 'GET':
+        data_query = Item.objects.all().values()
+        data = json.dumps(list(data_query))
 
-    response = HttpResponse(data)
-    response.headers['Content-Type'] = 'application/json'
-    return response
+        response = HttpResponse(data)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    elif request.method == 'POST':
+        # Note: request.POST doesn't work here
+        input_data = json.loads(request.body.decode())
+
+        new_item = Item(
+            value = input_data['title']
+        )
+        new_item.save()
+
+        return HttpResponse(json.dumps({'status': 'success'}))
